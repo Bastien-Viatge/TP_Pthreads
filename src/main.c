cbc81897c10f6326699bdd915a4e749ddc7a6d02
Ant-keke <kevin.antoine@insa-lyon.fr>
2014-03-18 14:42:02 +0100

    plein d erreurs

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
@@ -23,7 +24,7 @@ int is_prime( uint64_t p )
 void print_prime_factor( uint64_t n )
 {
 	char * c;
-	c+= (char *)n +" :";
+	c = strcat((char *)n," :");
 	uint64_t i = 2 ;
 	while( i<n)
 	{
@@ -31,7 +32,8 @@ void print_prime_factor( uint64_t n )
 			i++;
 		while( n%i == 0 )
 		{
-			c+=" "+ (char *)i;
+			c=strcat(c," ");
+			c=strcat(c,(char *)i);
 			n /= i;
 		}
 	if ( n%i != 0 )
@@ -40,23 +42,21 @@ void print_prime_factor( uint64_t n )
 	printf("%s\n",c);
 }
 
-void *pthreadMain()
+void *pthreadMainBoucle()
 //Starting function for threads
 //call print_prime_factor(i) for each number of the file
 {
-	//	pthread_t tid = pthread_self( );
-		// In order to enter in while : i = 1
+		pthread_t tid = pthread_self( );
+
 		uint64_t i = 1;
 		int nbtokens = 1;
-		while(nbtokens != 0 && nbtokens != EOF)
-		{
+
 			pthread_mutex_lock( &mutex);
 			nbtokens=fscanf(fichier,"%lu",&i);
 			pthread_mutex_unlock( &mutex);
 			print_prime_factor(i);
 			printf("nbtokens : %d \n",nbtokens);
-		}
-	pthread_exit( NULL);
+	pthread_exit( (void *)&nbtokens);
 }//Fin de pthread Main
 
 int main()
@@ -73,29 +73,34 @@ int main()
 		pthread_mutex_init(&mutex,NULL);
 
 		//---------------------------------------------Creating pthread
-		crdu = pthread_create( &pthread1, NULL, pthreadMain, NULL);
-		if ( crdu != 0 )
-		{	printf("Error : code=%d",crdu);
-		exit ( -1);
-		}
-		crdu = pthread_create( &pthread2,NULL, pthreadMain,NULL);
-		if ( crdu != 0 )
-		{	printf("Error : code=%d",crdu);
-		exit ( -1);
-		}
+		// In order to enter in while : nbtoki = 1
+		int * nbtok1 = 1;
+		int * nbtok2 = 1;
+		while(*nbtok1 >0 && *nbtok2>0)
+		{
+			crdu = pthread_create( &pthread1, NULL, pthreadMainBoucle, NULL);
+			if ( crdu != 0 )
+			{	printf("Error : code=%d",crdu);
+			exit ( -1);
+			}
+			crdu = pthread_create( &pthread2, NULL, pthreadMainBoucle,NULL);
+			if ( crdu != 0 )
+			{	printf("Error : code=%d",crdu);
+			exit ( -1);
+			}
 
-		//------------------------------------------------Join pthread
-		crdu = pthread_join(pthread1,NULL);
-		if ( crdu != 0 )
-		{	printf("Error : code=%d",crdu);
-		exit ( -1);
+			//------------------------------------------------Join pthread
+			crdu = pthread_join(pthread1,(int *)nbtok1);
+			if ( crdu != 0 )
+			{	printf("Error : code=%d",crdu);
+			exit ( -1);
+			}
+			crdu = pthread_join(pthread2,(int *)nbtok2);
+			if ( crdu != 0 )
+			{	printf("Erreur : code=%d",crdu);
+			exit ( -1);
+			}
 		}
-		crdu = pthread_join(pthread2,NULL);
-		if ( crdu != 0 )
-		{	printf("Erreur : code=%d",crdu);
-		exit ( -1);
-		}
-
 		//----------------------------------------Destruction du mutex
 		crdu = pthread_mutex_destroy( &mutex);
 		if ( crdu != 0 )
@@ -104,6 +109,6 @@ int main()
 		}
 
 		printf(" Pthread end\n");
-		pthread_exit ( NULL );
 	}
+	return 0;
 }
